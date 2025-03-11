5e23505dfed711ca922e2fa7a97ae7f7dfe45eff
Gebremariam Godad <godadgebremariam6@gmil.com>
2025-03-11 17:43:35 +0300

    first commit

---
+++ hello.cpp
@@ -0,0 +1,8 @@
+#include <iostream> // Includes the input/output stream library
+
+int main()
+{ // Entry point of the program
+    std::cout << "Hello, World! thisist" << std::endl;
+    std::cout << " hfgfhsd"; // Prints "Hello, World!" to the console
+    return 0;                // Returns 0 to the operating system
+}
